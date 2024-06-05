19415f43b84197631f797aa66fb338208af711fd
speedyfriend433 <speedyfriend13@gmail.com>
2024-06-05 12:01:29 +0900

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -18,81 +18,83 @@ struct ContentView: View {
     @State private var supportedRange: String? = nil
     @State private var supportedLinks: [String: String]? = nil
     
-     let trollStoreSupportData: [TrollStoreSupportData] = [
+    let trollStoreSupportData: [TrollStoreSupportData] = [
         TrollStoreSupportData(fromVersion: "14.0 beta 1 and earlier", toVersion: "14.0 beta 2", platforms: "arm64 (A8) - arm64 (A9-A11)", supported: [:]),
-        TrollStoreSupportData(fromVersion: "14.0 beta 2", toVersion: "14.8.1", platforms: "arm64 (A8) - arm64 (A9-A11)", supported: ["TrollInstallerX": "https://ios.cfw.guide/installing-trollstore-trollinstallerx", "TrollHelperOTA": "https://ios.cfw.guide/installing-trollstore-trollhelperota"]),
-        TrollStoreSupportData(fromVersion: "15.0", toVersion: "15.0", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollInstallerX": "https://ios.cfw.guide/installing-trollstore-trollinstallerx", "TrollHelperOTA": "https://ios.cfw.guide/installing-trollstore-trollhelperota"]),
-        TrollStoreSupportData(fromVersion: "15.0 beta 1", toVersion: "15.5 beta 4", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollHelperOTA": "https://ios.cfw.guide/installing-trollstore-trollhelperota"]),
-        TrollStoreSupportData(fromVersion: "15.5", toVersion: "15.5", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollInstallerMDC": "https://ios.cfw.guide/installing-trollstore-trollinstallermdc", "TrollInstallerX": "https://ios.cfw.guide/installing-trollstore-trollinstallerx", "TrollHelperOTA": "https://ios.cfw.guide/installing-trollstore-trollhelperota"]),
-        TrollStoreSupportData(fromVersion: "16.0 beta 1", toVersion: "16.0 beta 3", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: [:]),
+    TrollStoreSupportData(fromVersion: "14.0 beta 2", toVersion: "14.8.1", platforms: "arm64 (A8) - arm64 (A9-A11)", supported: ["TrollInstallerX": "https://ios.cfw.guide/installing-trollstore-trollinstallerx", "TrollHelperOTA": "https://ios.cfw.guide/installing-trollstore-trollhelperota"]),
+    TrollStoreSupportData(fromVersion: "15.0", toVersion: "15.0", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollInstallerX": "https://ios.cfw.guide/installing-trollstore-trollinstallerx", "TrollHelperOTA": "https://ios.cfw.guide/installing-trollstore-trollhelperota"]),
+    TrollStoreSupportData(fromVersion: "15.0 beta 1", toVersion: "15.5 beta 4", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollHelperOTA": "https://ios.cfw.guide/installing-trollstore-trollhelperota"]),
+    TrollStoreSupportData(fromVersion: "15.5", toVersion: "15.5", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollInstallerMDC": "https://ios.cfw.guide/installing-trollstore-trollinstallermdc", "TrollInstallerX": "https://ios.cfw.guide/installing-trollstore-trollinstallerx", "TrollHelperOTA": "https://ios.cfw.guide/installing-trollstore-trollhelperota"]),
+    TrollStoreSupportData(fromVersion: "16.0 beta 1", toVersion: "16.0 beta 3", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: [:]),
     TrollStoreSupportData(fromVersion: "16.0 beta 4", toVersion: "16.6.1", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollInstallerX": "https://ios.cfw.guide/installing-trollstore-trollinstallerx", "TrollHelperOTA": "https://ios.cfw.guide/installing-trollstore-trollhelperota"]),
     TrollStoreSupportData(fromVersion: "16.7 RC", toVersion: "16.7 RC", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollHelper": "https://ios.cfw.guide/installing-trollstore-trollhelper", "No Install Method": ""]),
     TrollStoreSupportData(fromVersion: "16.7", toVersion: "16.7.7", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["Unsupported": ""]),
     TrollStoreSupportData(fromVersion: "17.0 beta 1", toVersion: "17.0 beta 4", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollInstallerX": "https://ios.cfw.guide/installing-trollstore-trollinstallerx", "No Install Method": ""]),
     TrollStoreSupportData(fromVersion: "17.0 beta 5", toVersion: "17.0", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollHelper": "https://ios.cfw.guide/installing-trollstore-trollhelper", "No Install Method": ""]),
     TrollStoreSupportData(fromVersion: "17.0.1 and later", toVersion: "17.0.1 and later", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["Unsupported": ""])
-    ]
+]
     
     var body: some View {
-        VStack {
-            HStack {
-                Picker("Version Type", selection: $selectedVersionType) {
-                    ForEach(VersionType.allCases, id: \.self) {
-                        Text($0.rawValue)
-                    }
-                }
-                .pickerStyle(SegmentedPickerStyle())
-                .padding()
-                
-                Picker("Architecture", selection: $selectedArchitecture) {
-                    ForEach(Architecture.allCases, id: \.self) {
-                        Text($0.rawValue)
-                    }
-                }
-                .pickerStyle(SegmentedPickerStyle())
-                .padding()
-            }
-            
-            TextField("Type Your iOS Version", text: $iOSVersion)
-                .padding()
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            Button("Check") {
-                let result = trollStoreSupportInfo(for: iOSVersion)
-                isSupported = result.supported
-                supportedRange = result.supportedRange
-                supportedLinks = result.supportedLinks
-            }
-            .padding()
-            
-            if isSupported {
-                Text("iOS Version: \(iOSVersion)")
-                    .padding()
-                Text("TrollStore Support: Supported")
-                    .padding()
-                    .foregroundColor(.green)
-                if let range = supportedRange {
-                    Text(range)
-                        .padding()
-                }
-                if let links = supportedLinks {
-                    ForEach(links.sorted(by: <), id: \.key) { platform, link in
-                        Button("\(platform) Go to Install Guide") {
-                            guard let url = URL(string: link) else { return }
-                            UIApplication.shared.open(url)
+        NavigationView {
+            VStack {
+                Form {
+                    Section(header: Text("Version and Architecture")) {
+                        Picker("Version Type", selection: $selectedVersionType) {
+                            ForEach(VersionType.allCases, id: \.self) {
+                                Text($0.rawValue)
+                            }
                         }
-                        .padding()
+                        .pickerStyle(SegmentedPickerStyle())
+                        
+                        Picker("Architecture", selection: $selectedArchitecture) {
+                            ForEach(Architecture.allCases, id: \.self) {
+                                Text($0.rawValue)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                    }
+                    
+                    Section(header: Text("iOS Version")) {
+                        TextField("Enter iOS Version", text: $iOSVersion)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
+                    
+                    Section {
+                        Button("Check Support") {
+                            let result = trollStoreSupportInfo(for: iOSVersion)
+                            isSupported = result.supported
+                            supportedRange = result.supportedRange
+                            supportedLinks = result.supportedLinks
+                        }
+                    }
+                    
+                    if isSupported {
+                        Section(header: Text("Support Information")) {
+                            Text("iOS Version: \(iOSVersion)")
+                            Text("TrollStore Support: Supported")
+                                .foregroundColor(.green)
+                            if let range = supportedRange {
+                                Text(range)
+                            }
+                            if let links = supportedLinks {
+                                ForEach(links.sorted(by: <), id: \.key) { platform, link in
+                                    Button("\(platform) Installation Guide") {
+                                        guard let url = URL(string: link) else { return }
+                                        UIApplication.shared.open(url)
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        Section(header: Text("Support Information")) {
+                            Text("iOS Version: \(iOSVersion)")
+                            Text("TrollStore Support: Not Supported")
+                                .foregroundColor(.red)
+                        }
                     }
                 }
-            } else {
-                Text("iOS Version: \(iOSVersion)")
-                    .padding()
-                Text("TrollStore Supported: Unsupported")
-                    .padding()
-                    .foregroundColor(.red)
+                .navigationTitle("TrollStore Checker")
             }
         }
-        .padding()
     }
     
     func trollStoreSupportInfo(for iOSVersion: String) -> (supported: Bool, supportedRange: String?, supportedLinks: [String: String]?) {
@@ -101,7 +103,7 @@ struct ContentView: View {
         
         for data in trollStoreSupportData {
             if version >= data.fromVersion && version <= data.toVersion {
-                if let supportedPlatforms = data.supported[architecture] {
+                if let supportedPlatforms = data.supported[architecture], !supportedPlatforms.isEmpty {
                     return (true, "Supported platforms: \(supportedPlatforms), Full Range: \(data.fromVersion) - \(data.toVersion)", data.supported)
                 } else {
                     return (false, nil, nil)
@@ -116,7 +118,7 @@ struct TrollStoreSupportData {
     let fromVersion: String
     let toVersion: String
     let platforms: String
-    let supported: [String: String] 
+    let supported: [String: String] // Dictionary to hold supported platform and its link
 }
 
 struct ContentView_Previews: PreviewProvider {
