07b6f4a256f275a9b00d5e1808638d02ca9304b5
speedyfriend433 <speedyfriend13@gmail.com>
2024-06-05 20:20:00 +0900

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -16,19 +16,20 @@ struct ContentView: View {
     @State private var iOSVersion = ""
     @State private var isSupported = false
     @State private var supportedRange: String? = nil
-    @State private var supportedLinks: [String: String]? = nil
+    @State private var officialWebsites: [(String, String)]? = nil
+    @State private var installationGuides: [(String, String)]? = nil
     
     let trollStoreSupportData: [TrollStoreSupportData] = [
         TrollStoreSupportData(fromVersion: "14.0 beta 1", toVersion: "14.0 beta 2", platforms: "arm64 (A8) - arm64 (A9-A11)", supported: [:]),
-        TrollStoreSupportData(fromVersion: "14.0 beta 2", toVersion: "14.8.1", platforms: "arm64 (A8) - arm64 (A9-A11)", supported: ["TrollInstallerX": "https://ios.cfw.guide/installing-trollstore-trollinstallerx", "TrollHelperOTA": "https://ios.cfw.guide/installing-trollstore-trollhelperota"]),
-        TrollStoreSupportData(fromVersion: "15.0", toVersion: "15.0", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollInstallerX": "https://ios.cfw.guide/installing-trollstore-trollinstallerx", "TrollHelperOTA": "https://ios.cfw.guide/installing-trollstore-trollhelperota"]),
+        TrollStoreSupportData(fromVersion: "14.0 beta 2", toVersion: "14.8.1", platforms: "arm64 (A8) - arm64 (A9-A11)", supported: ["TrollInstallerX": "https://github.com/alfiecg24/TrollInstallerX/tree/1.0.2", "TrollHelperOTA": "https://ios.cfw.guide/installing-trollstore-trollhelperota"]),
+        TrollStoreSupportData(fromVersion: "15.0", toVersion: "15.0", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollInstallerX": "https://github.com/alfiecg24/TrollInstallerX/tree/1.0.2", "TrollHelperOTA": "https://ios.cfw.guide/installing-trollstore-trollhelperota"]),
         TrollStoreSupportData(fromVersion: "15.0 beta 1", toVersion: "15.5 beta 4", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollHelperOTA": "https://ios.cfw.guide/installing-trollstore-trollhelperota"]),
-        TrollStoreSupportData(fromVersion: "15.5", toVersion: "15.5", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollInstallerMDC": "https://ios.cfw.guide/installing-trollstore-trollinstallermdc", "TrollInstallerX": "https://ios.cfw.guide/installing-trollstore-trollinstallerx", "TrollHelperOTA": "https://ios.cfw.guide/installing-trollstore-trollhelperota"]),
+        TrollStoreSupportData(fromVersion: "15.5", toVersion: "15.5", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollHelperOTA": "https://ios.cfw.guide/installing-trollstore-trollhelperota", "TrollInstallerX": "https://github.com/alfiecg24/TrollInstallerX/tree/1.0.2", "TrollInstallerMDC": "https://dhinakg.github.io/apps.html"]),
         TrollStoreSupportData(fromVersion: "16.0 beta 1", toVersion: "16.0 beta 3", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: [:]),
-        TrollStoreSupportData(fromVersion: "16.0 beta 4", toVersion: "16.6.1", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollInstallerX": "https://ios.cfw.guide/installing-trollstore-trollinstallerx", "TrollHelperOTA": "https://ios.cfw.guide/installing-trollstore-trollhelperota"]),
+        TrollStoreSupportData(fromVersion: "16.0 beta 4", toVersion: "16.6.1", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollInstallerX": "https://github.com/alfiecg24/TrollInstallerX/tree/1.0.2", "TrollHelperOTA": "https://ios.cfw.guide/installing-trollstore-trollhelperota"]),
         TrollStoreSupportData(fromVersion: "16.7 RC", toVersion: "16.7 RC", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollHelper": "https://ios.cfw.guide/installing-trollstore-trollhelper", "No Install Method": ""]),
         TrollStoreSupportData(fromVersion: "16.7", toVersion: "16.7.7", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["Unsupported": ""]),
-        TrollStoreSupportData(fromVersion: "17.0 beta 1", toVersion: "17.0 beta 4", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollInstallerX": "https://ios.cfw.guide/installing-trollstore-trollinstallerx", "No Install Method": ""]),
+        TrollStoreSupportData(fromVersion: "17.0 beta 1", toVersion: "17.0 beta 4", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollInstallerX": "https://github.com/alfiecg24/TrollInstallerX/tree/1.0.2", "No Install Method": ""]),
         TrollStoreSupportData(fromVersion: "17.0 beta 5", toVersion: "17.0", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["TrollHelper": "https://ios.cfw.guide/installing-trollstore-trollhelper", "No Install Method": ""]),
         TrollStoreSupportData(fromVersion: "17.0.1 and later", toVersion: "17.0.1 and later", platforms: "arm64 (A8) - arm64e (A12-A17/M1-M2)", supported: ["Unsupported": ""])
     ]
@@ -56,7 +57,7 @@ struct ContentView: View {
                     Section(header: Text("iOS Version")) {
                         TextField("Enter iOS Version", text: $iOSVersion)
                             .textFieldStyle(RoundedBorderTextFieldStyle())
-                            .keyboardType(.decimalPad)
+                            .keyboardType(.default)
                     }
                     
                     Section {
@@ -64,7 +65,8 @@ struct ContentView: View {
                             let result = trollStoreSupportInfo(for: iOSVersion)
                             isSupported = result.supported
                             supportedRange = result.supportedRange
-                            supportedLinks = result.supportedLinks
+                            officialWebsites = result.officialWebsites
+                            installationGuides = result.installationGuides
                         }
                     }
                     
@@ -76,11 +78,25 @@ struct ContentView: View {
                             if let range = supportedRange {
                                 Text(range)
                             }
-                            if let links = supportedLinks {
-                                ForEach(links.sorted(by: <), id: \.key) { platform, link in
-                                    Button("\(platform) Installation Guide") {
-                                        guard let url = URL(string: link) else { return }
-                                        UIApplication.shared.open(url)
+                            
+                            if let officialWebsites = officialWebsites {
+                                Section(header: Text("Official Websites")) {
+                                    ForEach(officialWebsites, id: \.0) { (name, link) in
+                                        Button("\(name) Official Website") {
+                                            guard let url = URL(string: link) else { return }
+                                            UIApplication.shared.open(url)
+                                        }
+                                    }
+                                }
+                            }
+                            
+                            if let installationGuides = installationGuides {
+                                Section(header: Text("Installation Guides")) {
+                                    ForEach(installationGuides, id: \.0) { (name, link) in
+                                        Button("\(name) Installation Guide") {
+                                            guard let url = URL(string: link) else { return }
+                                            UIApplication.shared.open(url)
+                                        }
                                     }
                                 }
                             }
@@ -98,18 +114,38 @@ struct ContentView: View {
         }
     }
     
-    func trollStoreSupportInfo(for iOSVersion: String) -> (supported: Bool, supportedRange: String?, supportedLinks: [String: String]?) {
+    func trollStoreSupportInfo(for iOSVersion: String) -> (supported: Bool, supportedRange: String?, officialWebsites: [(String, String)]?, installationGuides: [(String, String)]?) {
         let version = selectedVersionType == .beta ? "\(iOSVersion) beta" : iOSVersion
         let architecture = selectedArchitecture.rawValue
         
         for data in trollStoreSupportData {
             if isVersionInRange(version, fromVersion: data.fromVersion, toVersion: data.toVersion) {
                 if data.platforms.contains(architecture) {
-                    return (!data.supported.isEmpty, "Supported from \(data.fromVersion) to \(data.toVersion)", data.supported)
+                    if !data.supported.isEmpty {
+                        var officialWebsites: [(String, String)] = []
+                        var installationGuides: [(String, String)] = []
+                        
+                        for (key, value) in data.supported {
+                            if key == "TrollInstallerX" {
+                                officialWebsites.append((key, "https://github.com/alfiecg24/TrollInstallerX/tree/1.0.2"))
+                                installationGuides.append((key, "https://ios.cfw.guide/installing-trollstore-trollinstallerx"))
+                            } else if key == "TrollInstallerMDC" {
+                                officialWebsites.append((key, "https://dhinakg.github.io/apps.html"))
+                                installationGuides.append((key, "https://ios.cfw.guide/installing-trollstore-trollinstallermdc/"))
+                            } else {
+                                officialWebsites.append((key, value))
+                                installationGuides.append((key, value))
+                            }
+                        }
+                        
+                        return (true, "Supported from \(data.fromVersion) to \(data.toVersion)", officialWebsites, installationGuides)
+                    } else {
+                        return (!data.supported.isEmpty, "Supported from \(data.fromVersion) to \(data.toVersion)", nil, nil)
+                    }
                 }
             }
         }
-        return (false, nil, nil)
+        return (false, nil, nil, nil)
     }
     
     func isVersionInRange(_ version: String, fromVersion: String, toVersion: String) -> Bool {
@@ -144,3 +180,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
